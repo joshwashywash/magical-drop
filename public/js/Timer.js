da6c66bfb6ba6641d757ec0225aa7a182908d9c9
joshwashywash <joshwashywash@gmail.com>
2018-08-02 22:31:43 -0500

    begin updates to documentation

---
+++ public/js/Timer.js
@@ -0,0 +1,42 @@
+/**
+ * modeled after https://github.com/meth-meth-method/game-timer
+ * @example
+ * const Timer = new Timer(() => (console.log('Hello World!')));
+ * timer.start();
+ */
+export default class Timer {
+    /**
+     * @param {Function} update callback for game updates
+     * @param {Number} dt resolution of game simulation
+     */
+    constructor(update, dt = 1 / 60) {
+      this._frame = 0;
+      let accumulated = 0;
+      let last = 0;
+      /**
+       * waits until time to update
+       * @param {Number} time since script has been initiated
+       */
+      this._update = (time) => {
+        accumulated += (time - last) / 1000;
+        while (accumulated > dt) {
+          update(dt);
+          accumulated -= dt;
+        }
+        last = time;
+        this.start();
+      };
+    }
+    /**
+     * ties requestAnimationFrame to class
+     */
+    start() {
+      this._frame = requestAnimationFrame(this._update);
+    }
+    /**
+     * cancels current update callback
+     */
+    stop() {
+      cancelAnimationFrame(this._frame);
+    }
+  }
